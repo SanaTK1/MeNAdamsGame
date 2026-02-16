0f159ab840e2c4c39fde080ea493fd5e820b0317
SanaTK1 <sana.hasheminia@gmail.com>
2026-02-16 14:21:36 -0500

    Turned magic number in PlayerFollow into parameter

--- Assets/Camera/Player Follow.cs
+++ Assets/Camera/Player Follow.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerFollow : MonoBehaviour
 {
     public float camera_speed = 8f;
+    public float yOffset = 10f;
     public Transform target;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,7 +13,7 @@ public class PlayerFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + 10, -10f);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
         transform.position = Vector3.Lerp(transform.position, newPos, camera_speed * Time.deltaTime);
     }
 }
