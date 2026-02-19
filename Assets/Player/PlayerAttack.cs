0003744a87f7741f1f4776d05cc06bb5c1b2a484
SanaTK1 <sana.hasheminia@gmail.com>
2026-02-19 15:31:47 -0500

    Implemented base logic for attacking

--- Assets/Player/PlayerAttack.cs
+++ Assets/Player/PlayerAttack.cs
@@ -1,25 +1,71 @@
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
     
     public Animator anim;
-
     public float cooldown = 0.5f;
     private float timer;
+    private InputAction attackButton;
+    private RaycastHit2D[] hits;
+
+    [SerializeField] private Transform attackTransform;
+    [SerializeField] private float attackRange = 1.5f;
+    [SerializeField] private LayerMask attackLayer;
+    [SerializeField] private float attackDamage = 1f;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        attackButton = InputSystem.actions.FindAction("attack");
+    }
 
     private void Update()
     {
+        //Checks to see if player wants to attack
+        if (attackButton.WasPressedThisFrame())
+        {
+            Attack();
+        }
+    }
+
+
+    private void Attack(){
+        //Acts almost like a net and catches all GameObjects that align based on given arguments
+            hits = Physics2D.CircleCastAll(attackTransform.position, 
+                                                    attackRange, 
+                                            transform.right,
+                                                    0f,
+                                                    attackLayer);
+            
+            //This is basically us sifting through the net of items we caught to see if there is anything valuable
+            for (int i = 0; i < hits.Length; i++)
+            {
+                IDamageable damageable = hits[i].collider.GetComponent<IDamageable>();
+                
+                //We then iterate and apply damage operations to items caught of interest
+                if (damageable != null)
+                {
+                    damageable.TakeDamage(attackDamage);
+                }
+            }
+            
         if (timer > 0)
         {
             timer -= Time.deltaTime;
         }
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackTransform.position, attackRange);
+    }
+
 
     //To be called by the Player movement script when attack button is pressed
-    public void attack()
+    /*public void attack()
     {
         if (timer <= 0)
         {
@@ -34,5 +80,5 @@ public class PlayerAttack : MonoBehaviour
     {
         anim.SetBool("IsAttacking", false);
     }
-
+*/
 }
